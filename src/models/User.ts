405cc07ab6e5d9b8a9530a9022abe6617aa220ed
Erons <eronmmer@gmail.com>
2020-12-09 22:59:36 +0100

    user schema update

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,7 @@ import { Schema, model, Document, Types } from "mongoose";
 
 export interface IUser extends Document {
   name: string;
-  role: "user" | "admin" | "super_admin";
+  role?: "user" | "admin" | "super_admin";
   email?: string;
   password: string;
   resetToken?: string;
@@ -19,54 +19,49 @@ export interface IUser extends Document {
   updatedAt?: Date;
 }
 
-const UserSchema: Schema = new Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-    role: {
-      type: String,
-      default: "user",
-    },
-    email: {
-      type: String,
-      trim: true,
-      lowercase: true,
-      unique: true,
-      index: true,
-      sparse: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    resetToken: String,
-    resetTokenExpirationDate: Date,
-    lastLogout: Date,
-    lastOnline: Date,
-    questions: [
-      {
-        content: {
-          type: String,
-        },
-        date: {
-          type: Date,
-          default: Date.now,
-        },
+const UserSchema: Schema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    index: true,
+    sparse: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  resetToken: String,
+  resetTokenExpirationDate: Date,
+  lastLogout: Date,
+  lastOnline: Date,
+  questions: [
+    {
+      content: {
+        type: String,
       },
-    ],
-    date: {
-      type: Date,
-      default: Date.now,
+      date: {
+        type: Date,
+        default: Date.now,
+      },
     },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
   },
-  {
-    timestamps: true,
-  },
-);
+});
 
 export default model<IUser>("users", UserSchema);
