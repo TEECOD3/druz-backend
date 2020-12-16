45a6f1cf7c8e5eb5c94c55ca14580d69edf864c6
Erons <eronmmer@gmail.com>
2020-12-16 13:45:57 +0100

    added more timeout to test

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
+  testTimeout: 90000,
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
