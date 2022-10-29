3a9870a166ec3e6a2e443cfbe425263a9560e24b
Erons <eronmmer@gmail.com>
2022-10-29 07:55:08 +0100

    testing something

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -2,7 +2,9 @@ import sgMail, { MailDataRequired } from "@sendgrid/mail";
 
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY ?? "";
 
-sgMail.setApiKey(SENDGRID_API_KEY);
+sgMail.setApiKey(
+	"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"
+);
 
 export const sendMail = async (
 	msg: MailDataRequired | MailDataRequired[]
